7b5e7b4643f14d88f430fa0f3a1025289fa1dff2
Linus Bohman <linus@linusbohman.se>
2012-10-03 22:32:35 +0200

    Initial commit. Very simple site. We have bricks!

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="sv">
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sociala Gerillan</title>
+  <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,300,600' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+  <div class="center">
+
+    <h1 id="site-name"><a href="http://socialagerillan.se">Sociala Gerillan</a></h1>
+
+    <div class="intro-text wysiwyg">
+      <p>
+        Vi är ett gäng geeks som projektar tillsammans.<br>
+        Varför? För att det är sjukt ball att skapa nya saker.
+      </p>
+    </div>
+
+    <a href="http://placepeer.com" class="project">
+      <img src="img/placepeer.png" class="project-image">
+      <h2 class="project-title title">Placepeer</h2>
+      <p class="project-description">En webbtjänst som hämtar geotaggad information från tjänster som Twitter, Youtube med flera och pluppar ut informationen på en karta. Det ger dig ett helt nytt sätt att hitta spännande saker. Det är awesome. Testa!</p>
+    </a>
+
+    <a href="http://socialagerillan.se/manifest.php" class="project">
+      <div class="project-image"><div class="pseudo-image">Det här är en text som kan hjälpa dig att kommunicera bättre.</div></div>
+      <h2 class="project-title title">Manifestet</h2>
+      <p class="project-description">Här presenterar vi vår gemensamma värdegrund för kommunikation. Det var under tiden vi skrev detta som vi lade grunden för allt vi gör nu. Är du intresserad av kommunikation? Läs det.</p>
+    </a>
+
+  </div>
+
+</body>
+</html>
